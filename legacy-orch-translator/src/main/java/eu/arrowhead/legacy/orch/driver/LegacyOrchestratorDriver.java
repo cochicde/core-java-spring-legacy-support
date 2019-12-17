33d7f629f85951d435aa674cb68c59fa6b8be327
borditamas <borditamas@gmail.com>
2019-12-17 10:55:40 +0100

    proceedOrchestration4.1.3 impl without token workaround

--- legacy-orch-translator/src/main/java/eu/arrowhead/legacy/orch/driver/LegacyOrchestratorDriver.java
+++ legacy-orch-translator/src/main/java/eu/arrowhead/legacy/orch/driver/LegacyOrchestratorDriver.java
@@ -1,9 +1,13 @@
 package eu.arrowhead.legacy.orch.driver;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -13,8 +17,10 @@ import org.springframework.web.util.UriComponents;
 
 import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.dto.shared.OrchestrationFlags.Flag;
 import eu.arrowhead.common.dto.shared.OrchestrationFormRequestDTO;
 import eu.arrowhead.common.dto.shared.OrchestrationResponseDTO;
+import eu.arrowhead.common.exception.BadPayloadException;
 import eu.arrowhead.common.http.HttpService;
 import eu.arrowhead.legacy.common.LegacyCommonConstants;
 import eu.arrowhead.legacy.common.model.LegacyOrchestrationResponse;
@@ -35,15 +41,17 @@ public class LegacyOrchestratorDriver {
 	@Value(CommonConstants.$SERVER_SSL_ENABLED_WD)
 	private boolean sslEnabled;
 	
+	private final Logger logger = LogManager.getLogger(LegacyOrchestratorDriver.class);
+	
 	//=================================================================================================
 	// methods
 
 	//-------------------------------------------------------------------------------------------------
 	public String echo() {
 		final String scheme = sslEnabled ? CommonConstants.HTTPS : CommonConstants.HTTP;
-		UriComponents uri = (UriComponents) arrowheadContext.get(LegacyCommonConstants.ORCHESTRATOR_ORCHESTRATION_URI);
-		String address = uri.getHost();
-		int port = uri.getPort();
+		final UriComponents uri = (UriComponents) arrowheadContext.get(LegacyCommonConstants.ORCHESTRATOR_ORCHESTRATION_URI);
+		final String address = uri.getHost();
+		final int port = uri.getPort();
 		final UriComponents uriEcho = Utilities.createURI(scheme, address, port, CommonConstants.ORCHESTRATOR_URI + CommonConstants.ECHO_URI);
 		final ResponseEntity<String> response = httpService.sendRequest(uriEcho, HttpMethod.GET, String.class);
 		
@@ -52,12 +60,40 @@ public class LegacyOrchestratorDriver {
 	
 	//-------------------------------------------------------------------------------------------------
 	public ResponseEntity<OrchestrationResponseDTO> proceedOrchestration413(final OrchestrationFormRequestDTO request) {
-		return null; //TODO
+		final String origin = CommonConstants.ORCHESTRATOR_URI + CommonConstants.OP_ORCH_PROCESS;
+		if (request.getOrchestrationFlags().get(Flag.TRIGGER_INTER_CLOUD)) {
+			throw new BadPayloadException("Translator does not support orchestration with flag 'TRIGGER_INTER_CLOUD=true'", HttpStatus.SC_BAD_REQUEST, origin);
+		}	
+		if (request.getOrchestrationFlags().get(Flag.ENABLE_INTER_CLOUD)) {
+			request.getOrchestrationFlags().put(Flag.ENABLE_INTER_CLOUD, false);
+			logger.debug("Orchestration flag 'ENABLE_INTER_CLOUD=true' is not supported and was changed to false");
+		}		
+		
+		final boolean originalMatchmakingFlag = request.getOrchestrationFlags().get(Flag.MATCHMAKING);
+		request.getOrchestrationFlags().put(Flag.MATCHMAKING, false);
+		
+		final UriComponents uri = (UriComponents) arrowheadContext.get(LegacyCommonConstants.ORCHESTRATOR_ORCHESTRATION_URI);		
+		final ResponseEntity<OrchestrationResponseDTO> response = httpService.sendRequest(uri, HttpMethod.POST, OrchestrationResponseDTO.class, request);
+		final OrchestrationResponseDTO dto = response.getBody();
+		
+		if (originalMatchmakingFlag && dto.getResponse().size() > 1) {
+			dto.setResponse(List.of(dto.getResponse().iterator().next()));
+		}
+		
+		return new ResponseEntity<>(dto, org.springframework.http.HttpStatus.OK);
 	}
 	
 	//-------------------------------------------------------------------------------------------------
 	public ResponseEntity<LegacyOrchestrationResponse> proceedOrchestration412(final LegacyServiceRequestForm request) {
 		return null; //TODO
+	}
+	
+	//=================================================================================================
+	// assistant methods
+	
+	//-------------------------------------------------------------------------------------------------
+	public void tokenWorkaround() {
+		//TODO
 	}
 
 }
